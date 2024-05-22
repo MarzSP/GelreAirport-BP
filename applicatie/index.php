94a572405596ad8573b0edebb764384c72744973
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-05-22 15:47:02 +0200

    added stylesheet nav

--- applicatie/index.php
+++ applicatie/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="navigation" href="../CSS/navigation.css">
+     <link rel="navigation" href="../CSS/nav.css">
   <link rel="stylesheet" href="../CSS/stylesheet.css">
     <title>GelreAirport</title>
 </head>
@@ -80,10 +80,10 @@
       </div>
     </div>
 
-  </main>
+  
 </body>
   <!-- Footer -->
   <footer>
     <p> 2024 Marianne Peterson S2136361</p>
-  </footer>
+  </footer></main>
 </html>
