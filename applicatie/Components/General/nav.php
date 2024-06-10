5609b50ec3b81be3f06df4b904a8cbbe6a2fe074
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-06-10 16:44:45 +0200

    Privacyverklaring

--- applicatie/Components/General/nav.php
+++ applicatie/Components/General/nav.php
@@ -6,12 +6,13 @@ $isLoggedin = isLoggedIn();
       <ul>
           <li><a href="../index.php">Home</a></li>
           <li><a href="../Views/passagier.php">Checkin</a></li>
-          <li><a href="../Views/contact.php">Contact</a></li>
+          
           <?php if ($isLoggedin) { ?>
              <li><?= $_SESSION['rol'].':'.$_SESSION['gebruikersnaam'] ?>(<a href="../Views/logout.php">Logout</a>)</li>
           <?php } else { ?>
             <li><a href="../Views/login.php">Login</a></li>
           <?php } ?>
+          <li><a href="../Views/Privacy.php">Privacy</a></li>
           <li><a href="../Views/medewerker.php">Medewerkertest</a></li>
           <li><a href="../Views/nieuwvlucht.php">nieuwvluchttest</a></li>
           <li><a href="../Views/nieuwpassagier.php">nieuwpassagiertest</a></li>
---
+++ applicatie/Views/privacy.php
@@ -0,0 +1,58 @@
+<?php require_once "../includes.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+  <link rel="stylesheet" href="../Styles/main.css">
+    <title>GelreAirport</title>
+</head>
+
+<body>
+<!-- Navigatie balk -->
+<?php include '../Components/General/nav.php';?>
+
+<!-- Pagina header -->
+<?php include '../Components/General/header.php';?>
+
+<main>
+<!-- Pagina header(column1), en welkomst text(column2) -->
+    <div class="leftcontainer">
+  
+<h3>Privacyverklaring Beroepsproduct (Leerproject en Onderzoek)</h1>
+<article>
+
+  <p>Ik, M.S.Peterson (S2136361) Hogeschool Arnhem Nijmegen, ("School", "ik", "mij", "we"), ben verantwoordelijk voor de verwerking van jouw persoonsgegevens in het kader van mijn leerproject en onderzoek met betrekking tot mijn website(s), applicatie(s) en andere online diensten (gezamenlijk "Diensten").</p>
+
+  <h4>Belangrijk:</h4>
+  <ul>
+    <li>Dit project is uitsluitend bedoeld voor leer- en onderzoeksdoeleinden.</li>
+    <li>Alle gegevens in dit project zijn fictief en gebaseerd op fictieve scenario's.</li>
+    <li>**Ik** verzamel, gebruik of deel **geen echte persoonsgegevens** van individuen of gebruikers.</li>
+  </ul>
+
+  <h4>Gegevensverzameling in dit Leerproject en Onderzoek</h4>
+  <p>Vanwege de fictieve aard van dit project, **verzamel ik geen echte persoonsgegevens** van jou of andere gebruikers. Alle gegevens die ik in het kader van dit project gebruik, zijn gefabriceerd en dienen uitsluitend voor educatieve en onderzoeksdoeleinden.</p>
+
+  <h4>Toestemming en Gebruikerscontrole</h4>
+  <p>Gezien de fictieve aard van dit project, heb je **geen expliciete toestemming** nodig om de gefabriceerde gegevens te gebruiken. De focus ligt op het leren van privacyprincipes en het onderzoeken van concepten met behulp van fictieve scenario's.</p>
+
+  <h4>Beveiliging van Gegevens</h4>
+  <p>Omdat er geen echte persoonsgegevens in dit project worden betrokken, hoef ik **geen specifieke beveiligingsmaatregelen** te nemen om gevoelige informatie te beschermen. De nadruk ligt op het gebruik van veilige praktijken tijdens het ontwikkelen en uitvoeren van het leerproject en onderzoek.</p>
+
+  <h4>Disclaimer</h4>
+  <p>Deze privacyverklaring is uitsluitend bedoeld voor educatieve en onderzoeksdoeleinden in het kader van dit fictieve schoolproject. Het mag **niet** worden beschouwd als een juridisch bindend document of een vervanging voor een officiële privacyverklaring voor een echte website of applicatie.</p>
+
+  <h4>Vragen en Contact</h4>
+  <p>Mocht je vragen hebben over deze privacyverklaring of het gebruik van gegevens in dit leerproject en onderzoek, aarzel dan niet om contact met me op te nemen via ms.peterson@student.han.nl</p>
+</article>
+</body>
+
+ <!-- Footer onderaan pagina -->
+ <?php
+include '../Components/General/footer.php';
+?>
+</main>
+</body>
+  </html>
