8c3ef8f4741c6151a8be8179cdc97cfce44c51f0
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-06-13 15:12:46 +0200

    Nav bar per rol

--- applicatie/Components/General/nav.php
+++ applicatie/Components/General/nav.php
@@ -1,20 +1,25 @@
 <!-- /includes/nav.php -->
 <?php
 $isLoggedin = isLoggedIn();
+$isLoggedin = isset($_SESSION['rol']);
+$rol = $_SESSION['rol'] ?? '';
 ?>
-    <nav> 
-      <ul>
-          <li><a href="../index.php">Home</a></li>
-          <li><a href="../Views/passagier.php">Checkin</a></li>
-          
-          <?php if ($isLoggedin) { ?>
-             <li><?= $_SESSION['rol'].':'.$_SESSION['gebruikersnaam'] ?>(<a href="../Views/logout.php">Logout</a>)</li>
-          <?php } else { ?>
+
+    <nav>
+    <ul>
+        <li><a href="../index.php">Home</a></li>
+        <?php if ($isLoggedin && $rol === 'medewerker') { ?>
+            <li><a href="../Views/medewerker.php">Zoeken</a></li>
+            <li><a href="../Views/nieuwvlucht.php">Vlucht toevoegen</a></li>
+            <li><a href="../Views/nieuwpassagier.php">Passagier toevoegen</a></li>
+        <?php } elseif ($isLoggedin && $rol === 'passagier') { ?>
+            <li><a href="../Views/passagier.php">Checkin</a></li>
+        <?php } ?>
+        <li><a href="../Views/Privacy.php">Privacy</a></li>
+        <?php if ($isLoggedin) { ?>
+            <li><?= htmlspecialchars($_SESSION['rol'], ENT_QUOTES, 'UTF-8') . ':' . htmlspecialchars($_SESSION['gebruikersnaam'], ENT_QUOTES, 'UTF-8') ?> (<a href="../Views/logout.php">Logout</a>)</li>
+        <?php } else { ?>
             <li><a href="../Views/login.php">Login</a></li>
-          <?php } ?>
-          <li><a href="../Views/Privacy.php">Privacy</a></li>
-          <li><a href="../Views/medewerker.php">Medewerkertest</a></li>
-          <li><a href="../Views/nieuwvlucht.php">nieuwvluchttest</a></li>
-          <li><a href="../Views/nieuwpassagier.php">nieuwpassagiertest</a></li>
-      </ul>
-    </nav>
+        <?php } ?>
+    </ul>
+</nav>
